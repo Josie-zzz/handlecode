1b49db693ab171e19ca6e4c6e4dce9a00ac28d1a
wangzengjie <1294450663@qq.com>
2022-06-19 23:15:03 +0800

    手写一个promise

--- promise.js
+++ promise.js
@@ -1,3 +1,165 @@
 /**
- * 手写promise
+ * 手写一个考虑比较周全的promise
  */
+
+// 定义三个状态常量
+/** 待决定状态（初始状态） */
+const PENDING = 'pending'
+/** 成功的状态 */
+const FULFILLED = 'fulfilled'
+/** 失败的状态 */
+const REJECTED = 'rejected'
+
+/** 这里判断和处理then的返回值，他可能是正常的对象，也可能是一个promise */
+const resolvePromise = (promise2, returnValue, resolve, reject) => {
+    if(promise2 === returnValue){
+        //总之是个报错的信息就行
+        reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
+    }
+
+    // 下面简化一下，文章里的逻辑看的不是很明白，很懵逼，下面自己来写下得了
+
+    // 如果这个返回值是promise的实例，就需要将这个promise的成功或者失败的参数传递到下一个then的参数中（就是promise2），注意第二个then是前面的then返回的新的promise2
+    // 那么如何传给下一个then的回调函数呢？
+    // 其实就是在这里调用then可以拿到这个promise成功或者失败的值，然后传递给promise2 的resolve or reject，这样promise2的then就会拿到这些值
+    if(returnValue instanceof Promise){
+        // returnValue.then((value) => resolve(value), reason => reject(reason)) 简写如下：
+        returnValue.then(resolve, reject)
+    } else {
+        // 如果只是简单的对象或者简单类型，就直接调用resolve，将值传递下去就行了
+        resolve(returnValue)
+    }
+
+}
+
+class Promise {
+    /** 参数executor译为执行器，需要立即执行的函数 */
+    constructor(executor) {
+        /** 当前promise的状态 */
+        this.status = PENDING
+        /** 储存成功的值 */
+        this.value = undefined
+        /** 储存失败的原因 */
+        this.reason = undefined
+        /** 储存成功的回调函数 */
+        this.onFulfilledCallbacks = []
+        /** 储存失败的回调函数 */
+        this.onRejectedCallbacks = []
+
+        /**
+         * 改变当前promise的状态为成功，并且储存成功的值，调用成功的回调函数
+         * 需要判断当前状态，只有待决定时才能执行这些，因为promise的状态只能被改变一次，下面同理
+         * @param {any} value 成功的值
+         */
+        const resolve = value => {
+            if (this.status === PENDING) {
+                this.status = FULFILLED
+                this.value = value
+
+                // 依次遍历执行成功的回调函数
+                this.onFulfilledCallbacks.forEach(callback => callback())
+            }
+        }
+
+        /**
+         * 改变当前promise的状态为失败，并且储存失败的原因，调用失败的回调函数
+         * @param {any} reason 失败的值
+         */
+        const reject = reason => {
+            if (this.status === PENDING) {
+                this.status = REJECTED
+                this.reason = reason
+
+                // 依次遍历执行失败的回调函数
+                this.onRejectedCallbacks.forEach(callback => callback())
+            }
+        }
+
+        // 立即执行executor函数，我们的业务代码，改变状态的逻辑都在这里面
+        try {
+            executor(resolve, reject)
+        } catch (e) {
+            reject(e)
+        }
+    }
+
+    then(onFulfilled, onRejected) {
+        // 防止回调函数没传的情况
+        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v
+        onRejected =
+            typeof onRejected === 'function'
+                ? onRejected
+                : err => {
+                      throw err
+                  }
+
+        //为了满足then的链式调用，所以then需要返回一个新的promise，同样这个给构造函数的参数会被立即执行
+        // 里面的代码都用try-catch包了起来，防止出错，但也降低了可读性
+        const promise2 = new Promise((resolve, reject) => {
+            // 如果调用then的时候状态是成功，例如反复调用一个状态已经更新的promise对象，下面失败也一样
+            if (this.status === FULFILLED) {
+                //注意这里用setTimeout 就纯粹是为了让resolvePromise拿到promise2的值，然后resolvePromise这里需要判断成功的结果不能是自己，
+                //因为新的promise2不可以等自己，这样就会死循环，自己等自己成功，这是有问题的。
+                //如果不加setTimeout那么在执行resolvePromise这个函数的时候promise2为undefined，这很好理解，因为执行这个函数的时候，构造函数还没结束，所以promise2还没接收到返回值
+                //真正的promise应该是用来另一种代替setTimeout的方式解决的问题，因为promise是微任务，setTimeout是宏任务
+                setTimeout(() => {
+                    try {
+                        let returnValue = onFulfilled(this.value)
+    
+                        // 处理这个返回值，因为这个返回值可能又是一个promise
+                        resolvePromise(promise2, returnValue, resolve, reject)
+                    } catch(e){
+                        reject(e)
+                    }
+                })
+            }
+
+            //简写了，和上面类似
+            if (this.status === REJECTED) {
+                let returnValue = onRejected(this.reason)
+                resolvePromise(promise2, returnValue, resolve, reject)
+            }
+
+            //如果调用then的时候状态是待决定，需要将成功的回调函数和失败的回调函数先存起来，等待状态变化的时候一一调用
+            //这里也简写了，和上面类似
+            if (this.status === PENDING) {
+                this.onFulfilledCallbacks.push(() => {
+                    const returnValue = onFulfilled(this.value)
+                    resolvePromise(promise2, returnValue, resolve, reject)
+                })
+
+                this.onRejectedCallbacks.push(() => {
+                    const returnValue = onRejected(this.reason)
+                    resolvePromise(promise2, returnValue, resolve, reject)
+                })
+            }
+        })
+
+        return promise2
+    }
+}
+
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(23)
+    }, 1000)
+})
+
+p.then(
+    value => {
+        console.log(value, 'value')
+        return new Promise((resolve, reject) => {
+            resolve(34)
+        }).then(s => {
+            console.log(s, 's')
+            return new Promise((resolve, reject) => {
+                resolve(99)
+            })
+        })
+    },
+    reason => {
+        console.log(reason, 'reason')
+    }
+).then(v => {
+    console.log(v, 'v')
+})
