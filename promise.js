46cb95ad2176167025ebc3d10794dbf95931e8f8
wangzengjie <1294450663@qq.com>
2022-06-26 19:19:48 +0800

    静态方法

--- promise.js
+++ promise.js
@@ -12,7 +12,7 @@ const REJECTED = 'rejected'
 
 /** 这里判断和处理then的返回值，他可能是正常的对象，也可能是一个promise */
 const resolvePromise = (promise2, returnValue, resolve, reject) => {
-    if(promise2 === returnValue){
+    if (promise2 === returnValue) {
         //总之是个报错的信息就行
         reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
     }
@@ -22,14 +22,13 @@ const resolvePromise = (promise2, returnValue, resolve, reject) => {
     // 如果这个返回值是promise的实例，就需要将这个promise的成功或者失败的参数传递到下一个then的参数中（就是promise2），注意第二个then是前面的then返回的新的promise2
     // 那么如何传给下一个then的回调函数呢？
     // 其实就是在这里调用then可以拿到这个promise成功或者失败的值，然后传递给promise2 的resolve or reject，这样promise2的then就会拿到这些值
-    if(returnValue instanceof Promise){
+    if (returnValue instanceof Promise) {
         // returnValue.then((value) => resolve(value), reason => reject(reason)) 简写如下：
         returnValue.then(resolve, reject)
     } else {
         // 如果只是简单的对象或者简单类型，就直接调用resolve，将值传递下去就行了
         resolve(returnValue)
     }
-
 }
 
 class Promise {
@@ -52,6 +51,14 @@ class Promise {
          * @param {any} value 成功的值
          */
         const resolve = value => {
+            // ------扩展方法，当value为一个promise的时候
+            // 这里是一个递归调用，如果是一个promise的话，这里的resolve的参数为这个promise的resolve的参数，能拿到这个参数当然要去then里面拿了
+            // 不要被return扰乱视线，它只是为了结束递归
+            if (value instanceof Promise) {
+                return value.then(resolve, reject)
+            }
+            // ------
+
             if (this.status === PENDING) {
                 this.status = FULFILLED
                 this.value = value
@@ -105,10 +112,10 @@ class Promise {
                 setTimeout(() => {
                     try {
                         let returnValue = onFulfilled(this.value)
-    
+
                         // 处理这个返回值，因为这个返回值可能又是一个promise
                         resolvePromise(promise2, returnValue, resolve, reject)
-                    } catch(e){
+                    } catch (e) {
                         reject(e)
                     }
                 })
@@ -137,12 +144,160 @@ class Promise {
 
         return promise2
     }
+
+    //catch就是基于then的
+    catch(callback) {
+        this.then(null, callback)
+    }
+
+    //这个finally是不管 Promise 对象最后状态如何，都会执行的操作
+    // 有两个点需要注意：
+    // 1. finally 的回调函数不接受参数
+    // 2. finally需要透传前面前面返回的参数
+    finally(callback) {
+        return this.then(
+            //其实可以看到这一坨是同步执行的：Promise.resolve(callback())
+            //主要在于这个then，这里返回的是一个promise，由上面的原理可以知道，这个then返回的value可以被下一个then接收到
+            //有点绕，需要多多理解下
+            value => Promise.resolve(callback()).then(() => value),
+            reason =>
+                Promise.resolve(callback()).then(() => {
+                    throw reason
+                })
+        )
+    }
+
+    //这两个静态方法其实蛮简单，重点在于扩展 constructor 中的 resolve, reject 这俩方法
+    // 这是因为value、reason很可能会是一个promise
+    static resolve(value) {
+        return new Promise((resolve, reject) => {
+            resolve(value)
+        })
+    }
+
+    static reject(reason) {
+        return new Promise((resolve, reject) => {
+            reject(reason)
+        })
+    }
+
+    // 用于处理并发的promise问题，接收一个数组，这里暂时模拟只接收数组的情况，官方提供还可以接收有Iterator接口的
+    static all(promiseArr) {
+        if (!Array.isArray(promiseArr)) {
+            return console.error('不是一个数组')
+        }
+
+        /** 储存所有结果的数组，注意这里的结果是按照promiseArr的顺序传的 */
+        let resultArr = []
+        /** 记录改变状态的promise数量 */
+        let index = 0
+
+        // 返回一个新的promise
+        return new Promise((resolve, reject) => {
+            promiseArr.forEach((p, i) => {
+                // 调用这个Promise.resolve方法也兼容非promise实例的情况，前面也看到如果是promise，则在resolve中会调用then，如果有错误也会抛出
+                Promise.resolve(p).then(
+                    value => {
+                        //储存成功的结果
+                        resultArr[i] = value
+                        // 记录成功的数量，加一
+                        index++
+
+                        // 如果所有的状态都成功了，就改变这个all返回的这个promise状态为成功
+                        if (index === promiseArr.length) {
+                            resolve(resultArr)
+                        }
+                    },
+                    err => {
+                        // 只要有一个错，就返回第一个报错的
+                        reject(err)
+                    }
+                )
+            })
+        })
+    }
+
+    static race(promiseArr){
+        if (!Array.isArray(promiseArr)) {
+            return console.error('不是一个数组')
+        }
+
+        return new Promise((resolve, reject) => {
+            promiseArr.forEach((p, i) => {
+                Promise.resolve(p).then(resolve, reject)
+            })
+        })
+    }
+
+    static allSettled(promiseArr){
+        if (!Array.isArray(promiseArr)) {
+            return console.error('不是一个数组')
+        }
+
+        return  new Promise((resolve, reject) => {
+            /** 储存所有结果的数组，不管成功还是失败，但是里面的数据是有个格式的 */
+            let resultArr = []
+            /** 记录改变状态的promise */
+            let index = 0
+            promiseArr.forEach((p, i) => {
+                Promise.resolve(p).then((value) => {
+                    resultArr[i] = {
+                        status: 'fulfilled',
+                        value,
+                    }
+                    index++
+
+                    // 不可以这样判断：promiseArr.length === resultArr.length，因为如果后面的先变化，例如：resultArr[3] = ...，那么resultArr.length 就是4，但是其中有空值，从而错误的进入这个判断
+                    if(index === promiseArr.length){
+                        resolve(resultArr)
+                    }
+                }, (reason) => {
+                    resultArr[i] = {
+                        status: 'rejected',
+                        reason,
+                    }
+
+                    if(index === promiseArr.length){
+                        resolve(resultArr)
+                    }
+                })
+            })
+        })
+    }
+
+    //如果有一个成功返回那就返回它，否则等待promise 都为reject的时候才结束，注意返回的可不是所有错误的集合
+    static any (promiseArr){
+        if (!Array.isArray(promiseArr)) {
+            return console.error('不是一个数组')
+        }
+
+        return  new Promise((resolve, reject) => {
+            /** 记录错误的promise数量 */
+            let index = 0
+
+            promiseArr.forEach((p, i) => {
+                Promise.resolve(p).then(resolve, (err) => {
+                    index++
+                    if(index == promiseArr.length){
+                        reject(new AggregateError('All promises were rejected'))
+                    }
+                })
+            })
+        })
+    }
 }
+
+// testing
 
 const p = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(23)
+        resolve(
+            new Promise(resolve => {
+                resolve(88888)
+            })
+        )
     }, 1000)
+    reject('dhjskh')
 })
 
 p.then(
@@ -163,3 +318,36 @@ p.then(
 ).then(v => {
     console.log(v, 'v')
 })
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({
+            code: 200
+        })
+    })
+    reject('error222')
+})
+
+Promise.all([p, p2]).then((val) => {
+    console.log(val, 'all')
+}, err => {
+    console.log(err, 'all')
+})
+
+Promise.race([p, p2]).then((val) => {
+    console.log(val, 'race')
+}, err => {
+    console.log(err, 'race')
+})
+
+Promise.allSettled([ p, p2]).then((val) => {
+    console.log(val, 'allSettled')
+})
+
+Promise.any([p, p2]).then((val) => {
+    console.log(val, 'any')
+}, err => {
+    console.log(err, 'any')
+})
+
+
